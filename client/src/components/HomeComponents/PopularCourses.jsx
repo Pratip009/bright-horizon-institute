4ae256da583e741ef3d9abca6441fa13f601ec67
Pratip Kayal <pratipkayal1@gmail.com>
2025-04-18 12:49:11 +0530

    commit

--- client/src/components/HomeComponents/PopularCourses.jsx
+++ client/src/components/HomeComponents/PopularCourses.jsx
@@ -51,7 +51,7 @@ export default function PopularCourses() {
 
       {/* Skeleton Cards Display */}
       {loading ? (
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-6">
           {Array(4)
             .fill(null)
             .map((_, index) => (
